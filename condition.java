6f2d39c02592b65aba4d38b9dd7d0c6f6ae78b1b
balajibadboy <balajicse005@gmail.com>
2018-05-29 21:56:02 -0700

    while and if else

---
+++ condition.java
@@ -0,0 +1,13 @@
+public class condition
+{
+public static void main(String args[])
+{
+int i=1;
+while(i<=50)
+{
+if(i%2!=0)
+System.out.println("odd");
+else
+System.out.println("even");
+i++;
+}} }
