a7cf4cdd0e11c2933ea89a900d672b47b9a9831c
balajibadboy <balajicse005@gmail.com>
2018-05-29 22:32:50 -0700

    switch

---
+++ switch1.java
@@ -0,0 +1,17 @@
+public class switch1
+{
+public static void main(String args[])
+{
+int ch=1;
+System.out.println(ch);
+switch(ch)
+{
+case 1: System.out.println("abb");
+break;
+case 2:System.out.println("bcc");
+break;
+default:break;
+}
+
+}
+}
